5d5fd7c00492df60383c6b040628589e513ab567
Mor Jenish <82307389+JenishMor@users.noreply.github.com>
2021-07-03 12:02:52 +0530

    Add files via upload

---
+++ Project10/index.js
@@ -0,0 +1,42 @@
+console.log("This is project 10 on flip a coin")
+
+let coin = document.getElementById('coin');
+let head = document.getElementById('Heading');
+
+coin.addEventListener('click', (e) => {
+    // console.log('You clicked on coin')
+    e.target.className += ' flip';
+    head.classList.remove('flip');
+    setTimeout(() => {
+        e.target.classList.remove('flip');
+        // console.log('Ok stop flipping')
+        let rand = Math.floor(Math.random() * 2);
+        // console.log(rand);
+
+        if (rand == 0) {
+            head.innerHTML = 'Head';
+        }
+        else if (rand == 1) {
+            head.innerHTML = 'Tail';
+        }
+    }, 2000);
+})
+
+let green = document.getElementById('green');
+let red = document.getElementById('red');
+let yellow = document.getElementById('yellow');
+
+green.addEventListener('click', ()=>{
+    // console.log('You clicked on green');
+    coin.style.backgroundColor = 'green';
+})
+
+red.addEventListener('click', ()=>{
+    // console.log('You clicked on red');
+    coin.style.backgroundColor = 'red';
+})
+
+yellow.addEventListener('click', ()=>{
+    // console.log('You clicked on yellow');
+    coin.style.backgroundColor = 'yellow';
+})
